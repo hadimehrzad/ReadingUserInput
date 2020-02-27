ad3c2ba218fb649769d57b648172d112b70b20ce
hadimehrzad <61123461+hadimehrzad@users.noreply.github.com>
2020-02-27 22:39:44 +0330

    Ex23: Reading User Input

---
+++ src/com/HMehrzad/Learning/Main.java
@@ -0,0 +1,29 @@
+package com.HMehrzad.Learning;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year of birth: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2018 - yearOfBirth;
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + " and you are " + age + " years old");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+            //System.out.println("Your name is " + name + " and you are " + age + " years old");
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
+        scanner.close();
+    }
+}
